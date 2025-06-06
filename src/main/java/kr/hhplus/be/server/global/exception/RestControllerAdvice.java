7c861e8bbf8b5d44d0f1ca7ad32d80a2de6a1e8e
TaeHoon <kimtahwn@naver.com>
2025-06-06 22:52:46 +0900

    [feat] 예외 및 상태 코드 추가

--- src/main/java/kr/hhplus/be/server/global/exception/RestControllerAdvice.java
+++ src/main/java/kr/hhplus/be/server/global/exception/RestControllerAdvice.java
@@ -1,10 +1,13 @@
 package kr.hhplus.be.server.global.exception;
 
+import kr.hhplus.be.server.coupon.domain.exception.CouponException;
 import kr.hhplus.be.server.global.dto.ApiResult;
+import kr.hhplus.be.server.global.dto.StatusCode;
 import kr.hhplus.be.server.point.domain.exception.PointException;
 import kr.hhplus.be.server.product.domain.exception.ProductException;
 import kr.hhplus.be.server.user.domain.exception.UserException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -33,5 +36,33 @@ public class RestControllerAdvice extends ResponseEntityExceptionHandler {
         return ResponseEntity
                 .status(e.getErrorCode().getStatus())
                 .body(ApiResult.error(e.getErrorCode().getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(value = CouponException.class)
+    public ResponseEntity<ApiResult<Void>> handleCouponException(CouponException e) {
+
+        return ResponseEntity
+                .status(e.getErrorCode().getStatus())
+                .body(ApiResult.error(e.getErrorCode().getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler({ IllegalArgumentException.class, MethodArgumentNotValidException.class })
+    public ResponseEntity<ApiResult<Void>> handleBadRequest(Exception e) {
+
+        StatusCode sc = StatusCode.BAD_REQUEST;
+
+        return ResponseEntity
+                .status(sc.getHttpStatus())
+                .body(ApiResult.error(sc.getCode(), sc.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResult<Void>> handleGenericException(Exception e) {
+
+        StatusCode sc = StatusCode.INTERNAL_SERVER_ERROR;
+
+        return ResponseEntity
+                .status(sc.getHttpStatus())
+                .body(ApiResult.error(sc.getCode(), sc.getMessage()));
     }
 }
